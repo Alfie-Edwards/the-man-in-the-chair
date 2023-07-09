85ef8fe3b30389ccf140bd340b8c8ad603fd860e
charlie-wt <cwt1g14@soton.ac.uk>
2023-07-09 17:41:50 +0100

    add all guards

--- game_state.lua
+++ game_state.lua
@@ -41,14 +41,41 @@ function GameState.new()
             SecurityCamera.new(103, 1, Direction.DOWN),
             SecurityCamera.new(112, 11, Direction.DOWN),
             Jukebox.new(0.5),
-            -- Guard.new(
-            --     {
-            --         {x = 64, y = 64},
-            --         {x = 128, y = 64},
-            --         {x = 128, y = 128},
-            --         {x = 64, y = 128}
-            --     }
-            -- ),
+            Guard.new(
+            {
+                { x = 672,  y = 320 },
+                { x = 592,  y = 224 },
+            }),
+            Guard.new(
+            {
+                { x = 512,  y = 96 },
+                { x = 512,  y = 224 },
+            }),
+            Guard.new(
+            {
+                { x = 912,  y = 352 },
+                { x = 912,  y = 240 },
+                { x = 1280, y = 240 },
+                { x = 912,  y = 240 },
+            }),
+            Guard.new(
+            {
+                { x = 1456, y = 208 },
+                { x = 848,  y = 208 },
+                { x = 832,  y =  64 },
+                { x = 1440, y =  64 },
+            }),
+            Guard.new(
+            {
+                { x = 1904, y = 224 },
+                { x = 416,  y = 224 },
+            }),
+            Guard.new(
+            {
+                { x = 1872, y = 336 },
+                { x = 1856, y = 224 },
+                { x = 1712, y = 224 },
+            }),
             George.new(),
         },
     })
