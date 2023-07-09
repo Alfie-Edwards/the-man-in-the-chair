ab5906c166359ed48069a5da121646ad67822a05
charlie-wt <cwt1g14@soton.ac.uk>
2023-07-09 11:33:28 +0100

    update hacking ui for new icons

--- ui/hacking.lua
+++ ui/hacking.lua
@@ -5,10 +5,12 @@ require "ui.image_button"
 Hacking = {
     MAX_POINTS = 3,
 
-    POINT_ICON_INSET_X = 20,
-    POINT_ICON_INSET_Y = 20,
-    POINT_ICON_SIZE_PX = 32,
-    POINT_ICON_SPACING = 10,
+    POINTS_INSET_X = 20,
+    POINTS_INSET_Y = 20,
+
+    POINT_ICON_SIZE_SCALE = 2,
+    POINT_ICON_SPACING = 2,
+    POINT_ICON_INSET_Y = 4,
 
     state = nil,
 
@@ -25,14 +27,15 @@ function Hacking.new(state)
 
     obj.points = Hacking.MAX_POINTS
 
+    -- hacking icons over each door
     for _,ntt in ipairs(obj.state.entities) do
         if is_type(ntt, Door) then
             local hack_door_button = ImageButton.new()
             local pos = ntt:pixel_pos(obj.state)
 
             hack_door_button:set_properties({
-                image = assets:get_image("ui/HackGreen"),
-                image_data = assets:get_image_data("ui/HackGreen"),
+                image = assets:get_image("ui/Hack1"),
+                image_data = assets:get_image_data("ui/Hack1"),
                 x_align = "left",
                 y_align = "bottom",
                 x = pos.x - obj.state.camera.x,
@@ -47,18 +50,45 @@ function Hacking.new(state)
         end
     end
 
+    -- points background banner
+    local points_bg = Image.new()
+    local points_bg_img = assets:get_image("ui/HackBG")
+    local points_bg_width = points_bg_img:getWidth() * Hacking.POINT_ICON_SIZE_SCALE
+    local points_bg_aspect = points_bg_img:getHeight() / points_bg_img:getWidth()
+    points_bg:set_properties({
+        image = points_bg_img,
+        image_data = assets:get_image_data("ui/HackBG"),
+        x_align = "left",
+        y_align = "top",
+        x = Hacking.POINTS_INSET_X,
+        y = Hacking.POINTS_INSET_Y,
+        width = points_bg_width,
+        height = points_bg_width * points_bg_aspect,
+    })
+    obj:add_child(points_bg)
+
+    -- individual point icons
+    local point_img = assets:get_image("ui/Hack1")
+    local point_img_width = point_img:getWidth() * Hacking.POINT_ICON_SIZE_SCALE
+    local point_img_aspect = point_img:getHeight() / point_img:getWidth()
+    -- horizontally centre the list of points over the background image
+    local total_points_width = point_img_width * Hacking.MAX_POINTS +
+                               Hacking.POINT_ICON_SPACING * (Hacking.MAX_POINTS - 1)
+    local centring_adjustment = Hacking.POINTS_INSET_X +
+                                (points_bg_width / 2) -
+                                (total_points_width / 2)
+
     for i=1,Hacking.MAX_POINTS do
         local point_icon = Image.new()
-        local img = assets:get_image("ui/HackGreen")
         point_icon:set_properties({
             image = img,
-            image_data = assets:get_image_data("ui/HackGreen"),
+            image_data = assets:get_image_data("ui/Hack1"),
             x_align = "left",
             y_align = "top",
-            x = Hacking.POINT_ICON_INSET_X + (i - 1) * (Hacking.POINT_ICON_SIZE_PX + Hacking.POINT_ICON_SPACING),
-            y = Hacking.POINT_ICON_INSET_Y,
-            width = Hacking.POINT_ICON_SIZE_PX,
-            height = Hacking.POINT_ICON_SIZE_PX,
+            x = centring_adjustment + (i - 1) * (point_img_width + Hacking.POINT_ICON_SPACING),
+            y = Hacking.POINTS_INSET_Y + Hacking.POINT_ICON_INSET_Y,
+            width = point_img_width,
+            height = point_img_width * point_img_aspect,
         })
         obj:add_child(point_icon)
 
@@ -74,8 +104,8 @@ function Hacking:use_point()
     end
 
     self.point_icons[self.points]:set_properties({
-        image = assets:get_image("ui/HackGrey"),
-        image_data = assets:get_image_data("ui/HackGrey"),
+        image = assets:get_image("ui/Hack2"),
+        image_data = assets:get_image_data("ui/Hack2"),
     })
 
     self.points = self.points - 1
@@ -87,8 +117,8 @@ function Hacking:restore_point()
     self.points = math.min(self.points + 1, Hacking.MAX_POINTS)
 
     self.point_icons[self.points]:set_properties({
-        image = assets:get_image("ui/HackGreen"),
-        image_data = assets:get_image_data("ui/HackGreen"),
+        image = assets:get_image("ui/Hack1"),
+        image_data = assets:get_image_data("ui/Hack1"),
     })
 
 end
